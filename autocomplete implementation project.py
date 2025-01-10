db61a724ce0d304dd30520497933edcec94468a1
55-coder <antonynjuguna502@gmail.com>
2025-01-10 13:18:17 +0300

    Added more code

--- autocomplete implementation project.py
+++ autocomplete implementation project.py
@@ -3,3 +3,8 @@ class TrieNode:
     def __init__(self):
         self.children = {}
         self.is_end_of_word = False  # Boolean flag to indicate if the node marks the end of a valid word
+
+class Trie:
+    # The main data structure
+    def __init__(self):
+        self.root = TrieNode()
